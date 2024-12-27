c9461270d7008d4b07f35919ea423056365223c2
daviscriptsOFFICIAL <112778327+Davicoderliner@users.noreply.github.com>
2024-12-27 12:03:20 -0300

    Create Example.lua

---
+++ Example.lua
@@ -0,0 +1,21 @@
+local DaviHub = loadstring(game:HttpGet("https://github.com/Davicoderliner/Davi-library-4.0-/blob/main/DaviHubLibrary.lua"))()
+
+local Window = DaviHub:CreateWindow("Davi Hub 4.0")
+
+local MainTab = Window:CreateTab("Main Features")
+
+MainTab:CreateButton("Speed Boost", function()
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 50
+end)
+
+MainTab:CreateToggle("Infinite Jump", function(state)
+    if state then
+        print("Infinite Jump Enabled")
+    else
+        print("Infinite Jump Disabled")
+    end
+end)
+
+MainTab:CreateSlider("WalkSpeed", 16, 100, 16, function(value)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = value
+end)
